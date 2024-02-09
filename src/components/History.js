69e311b9e67b0e7fb92723d3a5e0ba0b85c68446
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-09 22:17:46 +0300

    add build command

--- src/components/History.js
+++ src/components/History.js
@@ -3,12 +3,53 @@ import "./assets/css/History.css";
 import React, { useEffect, useState } from "react";
 
 export function HistoryForm() {
+
     const [data, setData] = useState(null);
-    
+    const idPersonne=localStorage.getItem('id');
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const idPersonne = localStorage.getItem('id');
+            const requestBody = {
+              idUser: idPersonne,
+            };
+            const response = await fetch(
+              `https://backend-production-b756.up.railway.app/parcellecultureparcelle/findParcelleCulturePersonneWithParametersById`,
+              {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody),
+              }
+            );
+      
+            if (response.ok) {
+              const responseData = await response.json();
+              setData(responseData);
+            } else {
+              console.warn("Server responded with an error:", response.status);
+              try {
+                const errorResponse = await response.json();
+                console.error("Server error details:", errorResponse);
+              } catch (error) {
+                console.error("Failed to parse server error details:", error);
+              }
+            }
+          } catch (error) {
+            console.error("An error occurred during the fetch:", error);
+          }
+        };
+      
+        fetchData();
+      }, []);
+      
+      
   return (
     <div className="content">
       <div className="title-h1">
-        <h1>Your simulation</h1>
+        <h1>Historique de simulation</h1>
       </div>
       <div className="table-wrapper">
         <table className="table-content">
