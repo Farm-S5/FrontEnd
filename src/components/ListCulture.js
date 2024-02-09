158dd741b2e76bdd6d3789558d61351e76cccd40
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-09 06:52:02 +0300

    add search

--- src/components/ListCulture.js
+++ src/components/ListCulture.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './assets/css/ListCulture.css';
+import React, { useEffect, useState } from "react";
+import "./assets/css/ListCulture.css";
 
 export function CultureForm() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://backend-production-b756.up.railway.app/parcellecultureparcelle/findParcelleCulturePersonne`
+        );
+
+        if (response.ok) {
+          const responseData = await response.json();
+          setData(responseData);
+        } else {
+          console.warn("Server responded with an error:", response.status);
+          try {
+            const errorResponse = await response.json();
+            console.error("Server error details:", errorResponse);
+          } catch (error) {
+            console.error("Failed to parse server error details:", error);
+          }
+        }
+      } catch (error) {
+        console.error("An error occurred during the fetch:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="content">
-      <div  className="title-h1">
+      <div className="title-h1">
         <h1>List of culture</h1>
       </div>
       <div className="table-wrapper">
@@ -12,36 +40,33 @@ export function CultureForm() {
           <thead className="head-content">
             <tr className="title-content">
               <th>OWNER</th>
-              <th colspan="2">CULTURE</th>
-              <th colspan="3">PARCEL</th>
+              <th colSpan="2">CULTURE</th>
+              <th colSpan="2">PARCEL</th>
             </tr>
             <tr className="subtitle-content">
               <th>Name</th>
-              <th>Culture name</th>
-              <th>Price</th>
-              <th>Yield</th>
-              <th>Size</th>
-              <th>Type of cultivation</th>
+              <th>Culture</th>
+              <th>Prix M²</th>
+              <th>id Parcelle</th>
+              <th>Superficie</th>
             </tr>
           </thead>
           <tbody className="body-content">
-            <tr className="text-content">
-              <td>Mihoby</td>
-              <td>Terrain</td>
-              <td>1000 Ar</td>
-              <td>2000 Ar</td>
-              <td>2 m</td>
-              <td>Champ agricole</td>
-            </tr>
-            <tr className="text-content">
-              <td>Mihoby</td>
-              <td>Terrain</td>
-              <td>1000 Ar</td>
-              <td>2000 Ar</td>
-              <td>2 m</td>
-              <td>Champ agricole</td>
-            </tr>
-            
+            {data ? (
+              data.map((item) => (
+                <tr className="text-content" key={item.idParcelle}>
+                  <td>{item.nomUser}</td>
+                  <td>{item.nomCulture}</td>
+                  <td>{item.prixM2}</td>
+                  <td>{item.idParcelle}</td>
+                  <td>{item.superficie}</td>
+                </tr>
+              ))
+            ) : (
+              <tr className="content">
+                <td colSpan="5">Loading...</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
@@ -50,4 +75,3 @@ export function CultureForm() {
 }
 
 export default CultureForm;
-
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './assets/css/Search.css';
 
 export function SearchForm() {
+    const [data, setData] = useState(null);
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `https://backend-production-b756.up.railway.app/parcellecultureparcelle/findParcelleCulturePersonne`
+          );
+  
+          if (response.ok) {
+            const responseData = await response.json();
+            setData(responseData);
+          } else {
+            console.warn("Server responded with an error:", response.status);
+            try {
+              const errorResponse = await response.json();
+              console.error("Server error details:", errorResponse);
+            } catch (error) {
+              console.error("Failed to parse server error details:", error);
+            }
+          }
+        } catch (error) {
+          console.error("An error occurred during the fetch:", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
     return (
         <div class="content-search">
             <h1 className="title-h1">Multi-Criteria Search</h1>
